a921668523899c01040361c604828552e5f99cac
seungbeom613 <jsw0613a@gmail.com>
2022-04-18 11:25:53 +0900

    로그인 화면 디자인

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { Background, style } from './LoginPageStyle';
+import { style } from './LoginPageStyle';
 export const LoginPage = () => {
   return (
     <Container>
       <Background></Background>
+      {/* -------------------------------- */}
+      <LeftForm>
+        <Text1>아직 계정이 없으신가요?</Text1>
+        <Text2>
+          간단한 회원가입을 통해
+          <br /> 첫 개발자 이력서를 작성해보세요!
+        </Text2>
+        <Btn1>회원가입</Btn1>
+      </LeftForm>
+      {/* -------------------------------- */}
+      <RightForm>
+        <Text3>로그인하기</Text3>
+        <Text4>
+          성공적인 개발자 취업을 위한
+          <br /> 가장 쉬운 이력서 제작 사이트
+        </Text4>
+        <EnterForm>
+          <Text5>이메일</Text5>
+          <Enter1></Enter1>
+          <Text6>비밀번호</Text6>
+          <Enter2 type="password"></Enter2>
+        </EnterForm>
+        <Btn2>로그인</Btn2>
+      </RightForm>
     </Container>
   );
 };
 
-const { Container } = style;
+const {
+  Container,
+  Background,
+  LeftForm,
+  Text1,
+  Text2,
+  Btn1,
+  RightForm,
+  Text3,
+  Text4,
+  EnterForm,
+  Text5,
+  Enter1,
+  Text6,
+  Enter2,
+  Btn2,
+} = style;
--- client/src/pages/LoginPage/LoginPageStyle.js
+++ client/src/pages/LoginPage/LoginPageStyle.js
@@ -3,21 +3,125 @@ import theme from 'styles/theme';
 
 export const Container = styled.div`
   ${({ theme }) => theme.flexSet()}
+  width:100%;
+  height: 100%;
+  position: fixed;
 `;
-
+// -------------------------------------------
 export const Background = styled.div`
   border-top: 1080px solid #334257;
   border-right: 400px solid transparent;
   width: 95%;
-  left: -30%;
+  left: -33%;
 
   overflow: hidden;
   position: absolute;
   top: 0;
   z-index: 0;
 `;
-
+// -------------------------------------------
+export const LeftForm = styled.div`
+  min-width: 300px;
+  z-index: 1;
+  color: white;
+  width: fit-content;
+  text-align: center;
+  margin-top: 5%;
+  margin-left: -4%;
+`;
+export const Text1 = styled.div`
+  font-size: 55px;
+  font-weight: bolder;
+`;
+export const Text2 = styled.div`
+  margin-top: 10%;
+  font-size: 32px;
+  font-weight: normal;
+  line-height: 150%;
+`;
+export const Btn1 = styled.button`
+  margin-top: 10%;
+  cursor: pointer;
+  border: white 3px solid;
+  border-radius: 5px;
+  color: white;
+  font-size: 32px;
+  width: 320px;
+  height: 80px;
+`;
+// -------------------------------------------
+export const RightForm = styled.div`
+  min-width: 300px;
+  width: fit-content;
+  z-index: 1;
+  color: black;
+  text-align: center;
+  margin-top: 3%;
+  margin-left: 25%;
+`;
+export const Text3 = styled.div`
+  font-size: 55px;
+  font-weight: bolder;
+`;
+export const Text4 = styled.div`
+  font-size: 32px;
+  font-weight: normal;
+  line-height: 150%;
+  margin-top: 5%;
+`;
+export const EnterForm = styled.div`
+  margin-top: 10%;
+  text-align: left;
+`;
+export const Text5 = styled.div`
+  font-size: 32px;
+`;
+export const Enter1 = styled.input`
+  width: 100%;
+  height: 50px;
+  border: black 1px solid;
+  border-radius: 10px;
+  font-size: 32px;
+  padding-left: 5px;
+`;
+export const Text6 = styled.div`
+  margin-top: 5%;
+  font-size: 32px;
+`;
+export const Enter2 = styled.input`
+  border: black 1px solid;
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  font-size: 32px;
+  padding-left: 5px;
+`;
+export const Btn2 = styled.button`
+  cursor: pointer;
+  width: 100%;
+  border: white 3px solid;
+  border-radius: 5px;
+  color: white;
+  font-size: 32px;
+  height: 80px;
+  background-color: #9e9e9e;
+  margin-top: 5%;
+`;
+// -------------------------------------------
 export const style = {
   Container,
   Background,
+  LeftForm,
+  Text1,
+  Text2,
+  Btn1,
+  RightForm,
+  Text3,
+  Text4,
+  EnterForm,
+  Text5,
+  Enter1,
+  Text6,
+  Enter2,
+  Btn2,
 };
