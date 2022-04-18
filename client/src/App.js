5fbdc1f4d309279492a5df7c171f4e9917c4cbf3
chaeyeon <pcyeon07@gmail.com>
2022-04-18 09:29:40 +0900

    Feat: Add LoginPage Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import LoginPage from 'pages/LoginPage';
 import MainPage from 'pages/MainPage';
 import StartPage from 'pages/StartPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StartPage />}></Route>
+        <Route path="/signin" element={<LoginPage />}></Route>
         <Route path="/main" element={<MainPage />}></Route>
       </Routes>
     </BrowserRouter>
