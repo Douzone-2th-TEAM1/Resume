e6eb0e6d83d7c03f1c94bacea3559fac28c2b22f
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-04-21 03:05:32 +0900

    #19 Feat: 임시 저장, 임시 저장 불러오기 구현

--- server/data/temps.js
+++ server/data/temps.js
@@ -21,3 +21,17 @@ export async function findByuId(u_id) {
     where: { u_id: u_id },
   });
 }
+
+// C UD 추가
+export async function createTemp(temp) {
+  return Temps.create(temp).then((data) => data.dataValues.u_id);
+}
+
+export async function updateTemp(temp) {
+  const { id, temp_data, u_id } = temp;
+  return Temps.update({ temp_data, u_id }, { where: { id }, returning: true });
+}
+
+export async function deleteTemp(u_id) {
+  return Temps.destroy({ where: { u_id } });
+}
