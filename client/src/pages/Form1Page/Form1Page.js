cdb6c1353e39f07d64c4559082d24cc944211e0c
seungbeom613 <jsw0613a@gmail.com>
2022-04-21 16:24:20 +0900

    이력서 폼 디자인

--- client/src/pages/Form1Page/Form1Page.js
+++ client/src/pages/Form1Page/Form1Page.js
@@ -1,19 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { style } from './Form1PageStyle';
+import { USERS } from 'utils/constants/users';
 
 export const Form1Page = () => {
   return (
     <Container>
-      <Headline />
-
-      <Profile>
-        <Profile_text>
-          <Text_name>김더존</Text_name>
-          <Text_phone>phone : 010-1234-2022</Text_phone>
-          <Text_emaiil>email : douzone@naver.com </Text_emaiil>
-        </Profile_text>
-        <Profile_img />
-      </Profile>
+      <Form>
+        <Headline />
+        {/* ------------------------- */}
+        <Profile>
+          <ProPhoto />
+          <ProText>
+            <TextName>{USERS.name}</TextName>
+            <TextPhone>phone : {USERS.phone}</TextPhone>
+            <TextEmail>e-mail : {USERS.email}</TextEmail>
+          </ProText>
+        </Profile>
+        {/* ------------------------- */}
+        <TopLeftForm>
+          <LeftIndex1>TECH SKILLS</LeftIndex1>
+          <LeftIndex2 />
+          <LeftIndex1>CERTIFICATIONS</LeftIndex1>
+          <LeftIndex2 />
+          <LeftIndex1>AWARDS</LeftIndex1>
+          <LeftIndex2 />
+          <LeftIndex1>CAREERS</LeftIndex1>
+          <LeftIndex2 />
+        </TopLeftForm>
+        {/* ------------------------- */}
+        <TopRightForm>
+          <EducationForm>
+            <EP_Logo>EDUCATION</EP_Logo>
+            <EP_Data />
+          </EducationForm>
+          <ProjectForm>
+            <EP_Logo>PROJECT</EP_Logo>
+            <EP_Data />
+          </ProjectForm>
+        </TopRightForm>
+        {/* ------------------------- */}
+        <BottomForm>
+          <FreeForm1>
+            <FF_Logo>지원동기</FF_Logo>
+            <FF_Data/>
+          </FreeForm1>
+          <FreeForm2>
+            <FF_Logo>입사 후 포부</FF_Logo>
+            <FF_Data/>
+          </FreeForm2>
+        </BottomForm>
+      </Form>
     </Container>
   );
 };
@@ -21,11 +57,25 @@ export const Form1Page = () => {
 // -----------------------------------------------------------
 const {
   Container,
+  Form,
   Headline,
   Profile,
-  Profile_img,
-  Profile_text,
-  Text_emaiil,
-  Text_name,
-  Text_phone,
+  ProPhoto,
+  ProText,
+  TextName,
+  TextPhone,
+  TextEmail,
+  TopLeftForm,
+  LeftIndex1,
+  LeftIndex2,
+  TopRightForm,
+  EducationForm,
+  EP_Logo,
+  EP_Data,
+  ProjectForm,
+  BottomForm,
+  FreeForm1,
+  FF_Logo,
+  FF_Data,
+  FreeForm2,
 } = style;
--- client/src/pages/Form1Page/Form1PageStyle.js
+++ client/src/pages/Form1Page/Form1PageStyle.js
@@ -1,61 +1,163 @@
 import styled from 'styled-components';
-import { keyframes } from 'styled-components';
-
+// -------------------------------------------
 export const Container = styled.div`
   ${({ theme }) => theme.flexSet()}
+  overflow: scroll;
 `;
 // -------------------------------------------
+export const Form = styled.div`
+  position: relative;
+  border: 1px solid black;
+  width: 1240px;
+  height: 1754px;
+  margin-left: 150px;
+  margin-top: 100px;
+`;
 export const Headline = styled.div`
-  height: 417px;
-  width: 100%;
+  width: 1238px;
+  height: 205px;
   background-color: #d6c000;
 `;
 // -------------------------------------------
 export const Profile = styled.div`
+  /* border: 1px black dotted; */
   position: absolute;
-  right: 100px;
-  top: 200px;
-  height: 352px;
-  width: 874px;
+  top: 50px;
+  right: 50px;
+  width: 647px;
+  height: 275px;
 `;
-export const Profile_text = styled.div`
-  position: absolute;
-  height: 352px;
-  width: 502px;
+export const ProPhoto = styled.div`
+  float: right;
+  border: 1px black solid;
+  width: 275px;
+  height: 275px;
+  background-color: white;
 `;
-export const Text_name = styled.div`
-  margin-top: 20px;
-  margin-bottom: 20px;
+export const ProText = styled.div`
+  width: 372px;
+  height: 275px;
+  color: white;
+`;
+export const TextName = styled.div`
+  text-align: center;
+  font-size: 48px;
+  font-weight: bolder;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+export const TextPhone = styled.div`
+  font-size: 24px;
+  margin-bottom: 5px;
+`;
+export const TextEmail = styled.div`
+  font-size: 24px;
+`;
+// -------------------------------------------
+export const TopLeftForm = styled.div`
+  /* border: 1px black dotted; */
+  width: 367px;
+  height: 980px;
+  margin-top: 100px;
+`;
+export const LeftIndex1 = styled.div`
+  width: 367px;
+  height: 59px;
+  background-color: #d6c000;
+  font-size: 40px;
+  font-weight: bolder;
   text-align: center;
   color: white;
-  font-weight: bolder;
+`;
+export const LeftIndex2 = styled.div`
+  height: 186px;
+  width: 367px;
+  font-size: 24px;
+`;
+// -------------------------------------------
+export const TopRightForm = styled.div`
+  /* border: 1px black dotted; */
+  position: absolute;
+  top: 250px;
+  right: 50px;
+  width: 780px;
+  height: 1108px;
+`;
+export const EducationForm = styled.div`
+  /* border: 1px black dotted; */
+  height: 529px;
+  margin-bottom: 50px;
+`;
+export const EP_Logo = styled.div`
+  /* border: 1px black dotted; */
+  height: 74px;
   font-size: 48px;
+  font-weight: bolder;
+  color: #d6c000;
 `;
-export const Text_phone = styled.div`
-  color: white;
-  font-size: 36px;
+export const EP_Data = styled.div`
+  height: 450px;
 `;
-export const Text_emaiil = styled.div`
-margin-top: 10px;
-  color: white;
-  font-size: 36px;
+export const ProjectForm = styled.div`
+  /* border: 1px black dotted; */
+  height: 529px;
 `;
-export const Profile_img = styled.div`
-  position: relative;
-  float: right;
-  height: 352px;
-  width: 352px;
-  border: 2px black solid;
-  background-color: white;
+// -------------------------------------------
+export const BottomForm = styled.div`
+  /* border: 1px black dotted; */
+  position: absolute;
+  bottom: 20px;
+  left: 2.5%;
+  height: 350px;
+  width: 95%;
+`;
+export const FreeForm1 = styled.div`
+  /* border: 1px black dotted; */
+  float: left;
+  margin-left: 50px;
+  width: 485px;
+  height: 100%;
+`;
+export const FF_Logo = styled.div`
+  /* border: 1px black dotted; */
+  font-size: 33px;
+  font-size: 48px;
+  font-weight: bolder;
+  color: #d6c000;
+`;
+export const FF_Data = styled.div`
+  /* border: 1px black dotted; */
+  margin-top: 10px;
+`;
+export const FreeForm2 = styled.div`
+  /* border: 1px black dotted; */
+  float: left;
+  margin-left: 100px;
+  width: 485px;
+  height: 100%;
 `;
 // -------------------------------------------
 export const style = {
   Container,
+  Form,
   Headline,
   Profile,
-  Profile_img,
-  Profile_text,
-  Text_emaiil,
-  Text_name,
-  Text_phone,
+  ProPhoto,
+  ProText,
+  TextName,
+  TextPhone,
+  TextEmail,
+  TopLeftForm,
+  LeftIndex1,
+  LeftIndex2,
+  TopRightForm,
+  EducationForm,
+  EP_Logo,
+  EP_Data,
+  ProjectForm,
+  BottomForm,
+  FreeForm1,
+  FF_Logo,
+  FF_Data,
+  FreeForm2,
 };
