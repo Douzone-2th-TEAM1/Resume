e0fe2acbb7a9195008beb3f218efc2179dc6540d
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-04-21 18:17:43 +0900

    #24 임시저장 관련 resCode:1 추가

--- server/controller/tempsController.js
+++ server/controller/tempsController.js
@@ -8,25 +8,28 @@ export async function saveTemp(req, res) {
   const temp_data = JSON.stringify(req.body);
   const found = await tempsTable.findByuId(id);
   if (found) {
-    await tempsTable.deleteTemp(id);
+    const temp = await tempsTable.deleteTemp(id);
   }
   const temp = await tempsTable.createTemp({
     u_id: id,
     temp_data,
   });
 
-  res.status(201).json({ resCode: 0 });
-}
+  res.json({ resCode: 0 });
 
 // 임시 저장 불러오기
 export async function loadTemp(req, res) {
   const id = req.id;
-  const { temp_data } = await tempsTable.findByuId(id);
-  const temp_data_JSON = temp_data.replaceAll(/'/g, '');
-  const data = JSON.parse(temp_data_JSON);
+  if (!(await tempsTable.findByuId(id))) {
+    res.json({ resCode: 1 });
+  } else {
+    const { temp_data } = await tempsTable.findByuId(id);
+    const temp_data_JSON = temp_data.replaceAll(/'/g, "");
+    const data = JSON.parse(temp_data_JSON);
 
-  // JSON으로 만든 temp_data 전송 추가해줘야함
-  res.status(201).json({ resCode: 0 });
+    // JSON으로 만든 data 전송 추가해줘야함
+    res.json({ resCode: 0 });
+  }
 }
 
 // 임시 저장 리스트
