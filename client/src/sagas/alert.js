5ceaa3144194938cb38d2773956babd8edf8d34d
chaeyeon <pcyeon07@gmail.com>
2022-04-22 09:09:09 +0900

    #18 Feat: alert redux-saga refactoring

--- client/src/sagas/alert.js
+++ client/src/sagas/alert.js
@@ -1,10 +1,11 @@
 import { all, call, fork, put, takeLatest, delay } from 'redux-saga/effects';
 import { AlertActionType } from 'myRedux/constants';
+import { closeAlert } from 'myRedux/actions/AlertActions';
 
 function* closeShowAlert() {
   try {
     yield delay(2000);
-    yield put({ type: AlertActionType.CLOSE_ALERT, status: false });
+    yield put(closeAlert());
   } catch (e) {
     console.log(e);
   }
