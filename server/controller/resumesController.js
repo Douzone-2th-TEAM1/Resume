2a2b7ad2ebfe22780ac5eb26d8d48a6ae9a67628
Donghun Lee <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-04-22 19:07:33 +0900

    Fix: 이력서 조회 수정

--- server/controller/resumesController.js
+++ server/controller/resumesController.js
@@ -76,43 +76,45 @@ export async function saveResume(req, res) {
   res.json({ resCode: 0, r_id: r_id });
 }
 
-// 이력서 리스트
-export async function findAllResumes(req, res) {
-  const id = req.id;
-  const resumes = await resumesTable.findAllById(id); // 사용자의 모든 이력서 리스트
-  res.json({ resCode: 0, resumes: resumes });
-}
-
 // 특정 이력서 조회
 export async function findResume(req, res) {
-  // 위의 이력서 리스트에서 원하는 r_id만 전송 받음
-  const { r_id } = req.body;
-  const { title, photo, department, portfolio, template } =
-    await resumesTable.findById(r_id);
-  const awards = await awardsTable.findAllById(r_id);
-  const careers = await careersTable.findAllById(r_id);
-  const certifications = await certificationsTable.findAllById(r_id);
-  const educations = await educationsTable.findAllById(r_id);
-  const projects = await projectsTable.findAllById(r_id);
-  const qnas = await qnasTable.findAllById(r_id);
-  const techs = await techsTable.findAllById(r_id);
+  const id = req.id;
+  const allRId = await resumesTable.findRId(id);
+  const resumesAll = [];
 
-  res.json({
-    // 양식 수정 필요
-    resCode: 0,
-    title,
-    photo,
-    department,
-    portfolio,
-    template,
-    awards,
-    careers,
-    certifications,
-    educations,
-    projects,
-    qnas,
-    techs,
-  });
+  for (let i = 0; i < allRId.length; i++) {
+    let r_id = allRId[i].r_id;
+    if (r_id) {
+      const { title, photo, department, portfolio, template, createdDate } =
+        await resumesTable.findById(r_id);
+      const awards = await awardsTable.findAllById(r_id);
+      const careers = await careersTable.findAllById(r_id);
+      const certifications = await certificationsTable.findAllById(r_id);
+      const educations = await educationsTable.findAllById(r_id);
+      const projects = await projectsTable.findAllById(r_id);
+      const qnas = await qnasTable.findAllById(r_id);
+      const techs = await techsTable.findAllById(r_id);
+
+      const resume = {
+        r_id,
+        title,
+        photo,
+        department,
+        portfolio,
+        template,
+        createdDate,
+        awards,
+        careers,
+        certifications,
+        educations,
+        projects,
+        qnas,
+        techs,
+      };
+      resumesAll.push(resume);
+    }
+  }
+  res.json({ resCode: 0, resumes: resumesAll });
 }
 
 // 이력서 삭제
